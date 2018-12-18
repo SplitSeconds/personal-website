61b0758ff22310f61df5d0d5364e47039993cad3
Nele Luepkes <nluepkes@gmail.com>
2018-12-18 17:18:33 +0100

    added cards

--- client/src/components/pages/Webdev.jsx
+++ client/src/components/pages/Webdev.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody,CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 class Webdev extends Component {
   constructor(props) {
@@ -10,11 +11,36 @@ class Webdev extends Component {
   }
   render() {
     return (
-      <div className="Secret">
-        <h2>Designs</h2>
-
-        <div className="result">
+      <div className="webdev">
+        <div className="headline">
           <h2>Hello Webdev</h2>
+        </div>
+
+        <div className="projects">
+
+        <div className="project-one">
+          <Card>
+            <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="project 1 picture" />
+            <CardBody>
+              <CardTitle>Card title</CardTitle>
+              <CardSubtitle>Card subtitle</CardSubtitle>
+              <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </div>
+
+        <div className="project-two">
+          <Card>
+            <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="project 1 picture" />
+            <CardBody>
+              <CardTitle>Card title</CardTitle>
+              <CardSubtitle>Card subtitle</CardSubtitle>
+              <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </div>
         </div>
 
         
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 // import registerServiceWorker from './registerServiceWorker';
